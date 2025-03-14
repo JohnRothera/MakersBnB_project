3089b8962e92cead756876a3fc0fcf20a7d6fc1a
Andrew <therealmon8@gmail.com>
2025-03-14 12:46:19 +0000

    delete 405 page and fixed space/id giving index error'
    \q
    
    asd

--- tests/test_route_spaces.py
+++ tests/test_route_spaces.py
@@ -7,3 +7,12 @@ def test_spaces_get(db_connection, web_client):
     response = web_client.get('/spaces')
     assert response.status_code == 200
     # did the page load??
+
+"""
+When: I go to makersbnb/spaces/123
+Then: I should get redirected to 404 page as it does not exist
+"""
+def test_spaces_123_get(db_connection, web_client):
+    db_connection.seed('seeds/makers_bnb_Bowie.sql')
+    response = web_client.get('/spaces/123')
+    assert response.status_code == 404
